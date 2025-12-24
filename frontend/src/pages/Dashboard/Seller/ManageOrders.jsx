036dbf385d7ebf3d4a9f57791d417c2a5bcf7e4c
shahidur-rahman18 <mdshahidurrahman3690@gmail.com>
2025-12-24 22:03:25 +0600

    decorator manage dashboard updated

--- frontend/src/pages/Dashboard/Seller/ManageOrders.jsx
+++ frontend/src/pages/Dashboard/Seller/ManageOrders.jsx
@@ -49,12 +49,7 @@ const ManageOrders = () => {
                     >
                       Quantity
                     </th>
-                    <th
-                      scope='col'
-                      className='px-5 py-3 bg-white  border-b border-gray-200 text-gray-800  text-left text-sm uppercase font-normal'
-                    >
-                      Address
-                    </th>
+                   
                     <th
                       scope='col'
                       className='px-5 py-3 bg-white  border-b border-gray-200 text-gray-800  text-left text-sm uppercase font-normal'
